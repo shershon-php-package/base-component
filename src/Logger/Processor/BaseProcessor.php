07269668a93072261259fcfdbd0099b4c4c44216
tanxiaoshan <18810877074@163.com>
2023-03-05 11:29:52 +0800

    update

--- src/Logger/Processor/BaseProcessor.php
+++ src/Logger/Processor/BaseProcessor.php
@@ -13,7 +13,7 @@ class BaseProcessor implements ProcessorInterface
         $message              = explode(':', $record['message'], 2);
         $record['module']     = count($message) > 1 ? $message[0] : '';
         $record['message']    = count($message) > 1 ? $message[1] : $record['message'];
-        $record['datetime']   = date('Y-m-d H:i:s', $arr[0]) . sprintf('%-03.3s', $arr[1] ?? '');
+        $record['datetime']   = date('Y-m-d H:i:s', $arr[0]) . '.' . sprintf('%-03.3s', $arr[1] ?? '');
         $record['request_id'] = REQUEST_ID;
         $record['use_time']   = round($microtime * 1000 - INDEX_START * 1000, 3);
         return $record;
